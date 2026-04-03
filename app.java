c23f136892a6f061d22c75486d53c163faa5b835
Bhaskar Paila <bhaskararaopaila19@gmail.com>
2026-04-03 11:58:30 +0530

    removed dev2 code

--- app.java
+++ app.java
@@ -6,13 +6,4 @@ public class HelloWorld {
 }
 
 
-//dev2 developde code
-public class Calculator {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
-        int sum = a + b;
-        
-        System.out.println("The sum is: " + sum); // Output: 15
-    }
-}
+
