06e6eb00c93258b693efd8f00437ec3abcdf5e34
Bhaskar Paila <bhaskararaopaila19@gmail.com>
2026-04-03 11:49:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.getElementById('colorBtn').addEventListener('click', function() {
     title.style.color = title.style.color === 'red' ? '#333' : 'red';
 });
 
+// adding java code for conflicts
+
+public class CheckNumber {
+    public static void main(String[] args) {
+        int number = 20;
+        
+        if (number > 15) {
+            System.out.println("The number is greater than 15.");
+        } else {
+            System.out.println("The number is 15 or less.");
+        }
+    }
+}
