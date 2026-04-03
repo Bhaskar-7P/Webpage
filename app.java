eb7e0ffd7f7aaaffb8831fda44ab1035d593afd1
Bhaskar Paila <bhaskararaopaila19@gmail.com>
2026-04-03 11:47:42 +0530

    Dev2 devloed code ios added

--- app.java
+++ app.java
@@ -4,3 +4,15 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+
+//dev2 developde code
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        
+        System.out.println("The sum is: " + sum); // Output: 15
+    }
+}
