92b320fc0848f90454eedc6404ec13cf2fbb5f10
Bhaskar <bhaskar@gmail.comm>
2026-04-03 04:56:53 +0000

    Added code in scripts and styles

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('colorBtn').addEventListener('click', function() {
+    const title = document.getElementById('main-title');
+    title.style.color = title.style.color === 'red' ? '#333' : 'red';
+});
+
